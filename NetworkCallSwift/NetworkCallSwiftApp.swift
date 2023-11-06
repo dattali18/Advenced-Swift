74b0a0c5d540b5cf015b55cbb0a6557b976e59ab
dattali18 <74492594+dattali18@users.noreply.github.com>
2023-11-06 16:49:13 +0200

    Initial Commit

---
+++ NetworkCallSwift/NetworkCallSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkCallSwiftApp.swift
+//  NetworkCallSwift
+//
+//  Created by Daniel Attali on 11/6/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkCallSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
